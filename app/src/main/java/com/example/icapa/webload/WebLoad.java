819caa3bee1a6a7f57356fc3e1b281065786115b
icapa <ivan.olea@gmail.com>
2016-11-21 20:16:18 +0100

    Initial commit

---
+++ app/src/main/java/com/example/icapa/webload/WebLoad.java
@@ -0,0 +1,16 @@
+package com.example.icapa.webload;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class WebLoad extends AppCompatActivity {
+    WebView wb;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_load);
+        wb = (WebView)findViewById(R.id.webView);
+        wb.loadUrl("http://www.google.es");
+    }
+}
