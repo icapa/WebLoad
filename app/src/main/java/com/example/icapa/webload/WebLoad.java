7962cc25b7e6f0be40e9a0b6f37aecb785df7938
icapa <ivan.olea@gmail.com>
2016-11-22 20:26:37 +0100

    Configuración de webclient

--- app/src/main/java/com/example/icapa/webload/WebLoad.java
+++ app/src/main/java/com/example/icapa/webload/WebLoad.java
@@ -3,6 +3,7 @@ package com.example.icapa.webload;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebLoad extends AppCompatActivity {
     WebView wb;
@@ -11,6 +12,7 @@ public class WebLoad extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_load);
         wb = (WebView)findViewById(R.id.webView);
+        wb.setWebViewClient(new WebViewClient());
         wb.loadUrl("http://www.google.es");
     }
 }
